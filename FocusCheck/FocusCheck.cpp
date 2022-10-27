845b712c03f7309cb9b8d891e0d9f27c3e22d9bc
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2022-10-27 20:13:59 +0900

    FocusCheck/FocusCheck.cpp

--- FocusCheck/FocusCheck.cpp
+++ FocusCheck/FocusCheck.cpp
@@ -10,10 +10,16 @@ int main(void)
         GetCursorPos(&pt);
         HWND hwnd = WindowFromPoint(pt);
         HWND hwndChild = ChildWindowFromPointEx(hwnd, pt, CWP_SKIPINVISIBLE);
+        CHAR szClass[128], szChildClass[128];
+        GetClassNameA(hwnd, szClass, _countof(szClass));
+        GetClassNameA(hwndChild, szChildClass, _countof(szChildClass));
+        CHAR szText[128], szChildText[128];
+        GetWindowTextA(hwnd, szText, _countof(szText));
+        GetWindowTextA(hwndChild, szChildText, _countof(szChildText));
 
         printf("---\n");
-        printf("hwnd: %p\n", (void *)hwnd);
-        printf("hwndChild: %p\n", (void *)hwndChild);
+        printf("hwnd: %p (%s) - '%s'\n", (void *)hwnd, szClass, szText);
+        printf("hwndChild: %p (%s) - '%s'\n", (void *)hwndChild, szChildClass, szChildText);
         printf("GetForegroundWindow(): %p\n", (void *)GetForegroundWindow());
         printf("GetFocus(): %p\n", (void *)GetFocus());
         printf("GetActiveWindow(): %p\n", (void *)GetActiveWindow());
